8621f13293f811d5d525666e95b96df74e7e37c4
Meme <meme_es@hotmail.com>
2021-03-31 18:06:11 -0600

    Fix the forgotten password mechanism
    Using the Laravel mechanism to send tokens when a user forgot its
    password, let users unactive to do that, this commit fixes the action,
    if a user is not active, the system doesn't send a mail with a token to
    retrieve the password.

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
@@ -19,4 +22,25 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email|exists:users']);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user->active) {
+            return back()->with('error', 'Su cuenta esta deshabilitada, le sugerimos comunicarse con su administrador');
+        } else {
+            // We will send the password reset link to this user. Once we have attempted
+            // to send the link, we will examine the response then see the message we
+            // need to show to the user. Finally, we'll send out a proper response.
+            $response = $this->broker()->sendResetLink(
+                $this->credentials($request)
+            );
+
+            return $response == Password::RESET_LINK_SENT
+                        ? $this->sendResetLinkResponse($request, $response)
+                        : $this->sendResetLinkFailedResponse($request, $response);
+        }
+    }
 }
