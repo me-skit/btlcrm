d7b6ba468f99148d0cb2380ddc9ff7fb1222e759
Meme <meme_es@hotmail.com>
2022-09-12 15:15:30 -0600

    Improve the members view

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -5,26 +5,32 @@
     <!-- the query thing -->
     <div class="row justify-content-center mb-3">
       <div class="col-md-10 d-flex justify-content-between align-items-baseline">
-        <h2 id="title" data-name="members"><i class="fas fa-users"></i> Miembros</h2>
-        <div>
-          <a href="{{ route('members.index') }}" class="btn btn-light"><i class="fas fa-redo-alt"></i> Recargar</a>
+        <h3 id="title" data-name="{{ $address }}"><i class="fas fa-users"></i> {{ $title }}</h3>
+        <div class="d-print-none">
+          <a href="{{ url($address) }}" class="btn btn-light"><i class="fas fa-redo-alt"></i><span class="d-none d-lg-inline"> Recargar</span></a>
+          <a id="btn-print" href="#" class="btn btn-light"><i class="fas fa-print"></i><span class="d-none d-lg-inline"> Imprimir</span></a>
+          {{-- <a href="{{ route('members.index') }}" class="btn btn-light"><i class="fas fa-redo-alt"></i> Recargar</a> --}}
         </div>
       </div>
     </div>
 
-    <div class="row form-group">
-      <div class="col-sm-7 col-md-5 col-lg-4">
-        <div class="input-group">
-          <input type="text"
-            name="search"
-            id="search"
-            class="form-control border-right-0 border @error('search') is-invalid @enderror"
-            value="{{ old('search') }}"
-            autofocus
-            >
-          <span class="input-group-append">
-            <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
-          </span>
+    <div class="row justify-content-center d-print-none">
+      <div class="col-md-10">
+        <div class="row form-group">
+          <div class="col-sm-8 col-md-7 col-lg-5">
+            <div class="input-group">
+              <input type="text"
+                name="search"
+                id="search"
+                class="form-control border-right-0 border @error('search') is-invalid @enderror"
+                value="{{ old('search') }}"
+                autofocus
+                >
+              <span class="input-group-append">
+                <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- resources/views/people/pagination.blade.php
+++ resources/views/people/pagination.blade.php
@@ -1,35 +1,39 @@
-<div class="row">
-  <div class="col-md-12">
+<div class="row justify-content-center d-print-none">
+  <div class="col-md-10">
     {{ $people->links("pagination::bootstrap-4") }}
   </div>
 </div>
 
 <div class="row justify-content-center">
-  <div class="col-md-12">
+  <div class="col-md-10">
     <table class="table table-sm table-hover table-responsive-md">
       <thead>
         <tr>
           <th class="text-center">No.</th>
           <th>Nombre</th>
-          <th>Acept.</th>
-          <th>Baut.</th>
-          <th>Acciones</th>
+          <th>A</th>
+          <th>B</th>
+          <th class="text-center">Edad</th>
+          <th class="text-center">EC</th>
+          <th class="d-print-none">Acciones</th>
         </tr>
       </thead>
       <tbody>
         @foreach ($people as $key => $person)
         <tr>
-          <td class="align-middle text-center">{{ ($people->currentPage() - 1) * 7 + $key + 1 }}</td>
+          <td class="align-middle text-center">{{ ($people->currentPage() - 1) * $people->perPage() + $key + 1 }}</td>
           <td class="align-middle text-truncate">
             {{ $person->full_name }} 
           </td>
           <td class="align-middle">{{ $person->accepted ? "Si" : "No" }}</td>
           <td class="align-middle">{{ $person->baptized ? "Si" : "No" }}</td>
-          <td class="align-middle">
+          <td class="align-middle text-center">{{ $person->age }}</td>
+          <td class="align-middle text-center">{{ $person->civil_status }}</td>
+          <td class="align-middle d-print-none">
             <div class="d-flex">
-              <a href="{{ route('person.show', $person->id ) }}" class="btn btn-secondary mr-3"><i class="far fa-eye"></i><span class="d-none d-lg-inline"> Detalles</span></a>
-              <a href="{{ route('person.edit', $person->id ) }}" class="btn btn-primary mr-3"><i class="fas fa-pencil-alt"></i><span class="d-none d-lg-inline"> Editar</span></a>
-              <a href="{{ route('family.show', $person->family()->id ) . '?back=members' }}" class="btn btn-outline-primary mr-3"><i class="fas fa-house-user"></i><span class="d-none d-lg-inline"> Familia</span></a>
+              <a href="{{ route('person.show', $person->id ) }}" class="btn btn-sm btn-secondary mr-3"><i class="far fa-eye"></i><span class="d-none d-lg-inline"> Detalles</span></a>
+              <a href="{{ route('person.edit', $person->id ) }}" class="btn btn-sm btn-primary mr-3"><i class="fas fa-pencil-alt"></i><span class="d-none d-lg-inline"> Editar</span></a>
+              <a href="{{ route('family.show', $person->family()->id ) . '?back=' . $address }}" class="btn btn-sm btn-outline-primary mr-3"><i class="fas fa-house-user"></i><span class="d-none d-lg-inline"> Familia</span></a>
             </div>
           </td>
         </tr>
